cc81f8dcb109c1c48685d8e841df5cf02cdafd97
mattias <mattias@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2009-01-05 15:30:07 +0000

    Tagged this date
    git-svn-id: https://svn.neo4j.org/components@2551 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ index-util/tags/0.6-2009-01-05/src/test/java/org/neo4j/util/sortedtree/TestSortedTree.java
@@ -0,0 +1,89 @@
+package org.neo4j.util.sortedtree;
+
+import java.util.Comparator;
+
+import org.neo4j.api.core.Node;
+import org.neo4j.util.NeoTestCase;
+import org.neo4j.util.btree.BTree.RelTypes;
+import org.neo4j.util.sortedtree.SortedTree;
+
+public class TestSortedTree extends NeoTestCase
+{
+	private SortedTree bTree;
+	
+	@Override
+	public void setUp() throws Exception
+	{
+	    super.setUp();
+		Node bNode = neo().createNode();
+		neo().getReferenceNode().createRelationshipTo( bNode, 
+			RelTypes.TREE_ROOT );
+		bTree = new SortedTree( neo(), bNode, new NodeSorter() );
+	}
+
+    private static final String VALUE = "value";
+    
+    static class NodeSorter implements Comparator<Node>
+    {
+        public int compare( Node o1, Node o2 )
+        {
+            Comparable c1 = (Comparable) o1.getProperty( VALUE );
+            Comparable c2 = (Comparable) o2.getProperty( VALUE );
+            return c1.compareTo( c2 );
+        }
+    }
+    
+	@Override
+	public void tearDown() throws Exception
+	{
+ 		bTree.delete();
+ 		super.tearDown();
+	}
+    
+    public void testBasicSort()
+    {
+        bTree.addNode( createNode( 'c' ) );
+        bTree.addNode( createNode( 'n' ) );
+        bTree.addNode( createNode( 'g' ) );
+        bTree.addNode( createNode( 'a' ) );
+        bTree.addNode( createNode( 'h' ) );
+        bTree.addNode( createNode( 'e' ) );
+        bTree.addNode( createNode( 'k' ) );
+        bTree.addNode( createNode( 'i' ) );
+
+        bTree.addNode( createNode( 'q' ) );
+        bTree.addNode( createNode( 'm' ) );
+        bTree.addNode( createNode( 'f' ) );
+        bTree.addNode( createNode( 'w' ) );
+        bTree.addNode( createNode( 'l' ) );
+        bTree.addNode( createNode( 't' ) );
+        bTree.addNode( createNode( 'z' ) );
+        
+        bTree.addNode( createNode( 'd' ) );
+        bTree.addNode( createNode( 'p' ) );
+        bTree.addNode( createNode( 'r' ) );
+        bTree.addNode( createNode( 'x' ) );
+        bTree.addNode( createNode( 'y' ) );
+        bTree.addNode( createNode( 's' ) );
+        
+        bTree.addNode( createNode( 'b' ) );
+        bTree.addNode( createNode( 'j' ) );
+        bTree.addNode( createNode( 'o' ) );
+        bTree.addNode( createNode( 'u' ) );
+        bTree.addNode( createNode( 'v' ) );
+
+        char c = 'a';
+        for ( Node node : bTree.getSortedNodes() )
+        {
+            assertEquals( c, node.getProperty( VALUE ) );
+            c++;
+        }
+    }
+    
+    public Node createNode( char c )
+    {
+        Node node = neo().createNode();
+        node.setProperty( VALUE, c );
+        return node;
+    }
+}
