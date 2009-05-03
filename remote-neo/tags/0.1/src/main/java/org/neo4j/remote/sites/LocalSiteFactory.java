02429bc15a6607e051fa2261567a8b9022fdd77c
tobias <tobias@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2009-05-03 20:56:45 +0000

    Creating a release tag for Remote Neo 0.1.
    Also bumping up the version in trunk to 0.2-SNAPSHOT.
    
    
    git-svn-id: https://svn.neo4j.org/components@2824 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ remote-neo/tags/0.1/src/main/java/org/neo4j/remote/sites/LocalSiteFactory.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2008-2009 Network Engine for Objects in Lund AB [neotechnology.com]
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.remote.sites;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.neo4j.api.core.EmbeddedNeo;
+import org.neo4j.api.core.NeoService;
+import org.neo4j.remote.RemoteSite;
+import org.neo4j.remote.RemoteSiteFactory;
+
+/**
+ * A {@link RemoteSiteFactory} that creates {@link LocalSite}s.
+ * @author Tobias Ivarsson
+ */
+public final class LocalSiteFactory extends RemoteSiteFactory
+{
+    /**
+     * Create a new {@link RemoteSiteFactory} for the file:// protocol.
+     */
+    public LocalSiteFactory()
+    {
+        super( "file" );
+    }
+
+    @Override
+    protected RemoteSite create( URI resourceUri )
+    {
+        return new LocalSite( getNeoService( new File( resourceUri
+            .getSchemeSpecificPart() ) ) );
+    }
+
+    @Override
+    protected boolean handlesUri( URI resourceUri )
+    {
+        return "file".equals( resourceUri.getScheme() );
+    }
+
+    private static Map<String, NeoService> instances = new HashMap<String, NeoService>();
+    
+    static synchronized NeoService getNeoService( File file )
+    {
+    	String path;
+    	try {
+    		path = file.getCanonicalPath();
+    	} catch ( IOException ex ) {
+    		path = file.getAbsolutePath();
+    	}
+        NeoService neo = instances.get( path );
+        if ( neo == null )
+        {
+            neo = newNeoService( path );
+            instances.put( path, neo );
+        }
+        return neo;
+    }
+
+    private static NeoService newNeoService( String path )
+    {
+        final NeoService neo = new EmbeddedNeo( path );
+        Runtime.getRuntime().addShutdownHook( new Thread( new Runnable()
+        {
+            public void run()
+            {
+                neo.shutdown();
+            }
+        } ) );
+        return neo;
+    }
+}
