479f1a95e2211288411c3be9a2f16ba20b575a4d
johans <johans@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2009-08-15 16:56:23 +0000

    Tagged index-util 0.7 and bumped version in trunk.
    git-svn-id: https://svn.neo4j.org/components@3048 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ index-util/tags/0.7/src/test/java/org/neo4j/util/index/TestBatchInsert.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2002-2008 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ * 
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.util.index;
+
+import java.io.File;
+
+import junit.framework.TestCase;
+
+import org.neo4j.impl.batchinsert.BatchInserter;
+import org.neo4j.impl.batchinsert.BatchInserterImpl;
+
+public class TestBatchInsert extends TestCase
+{
+    @Override
+    public void tearDown() throws Exception
+    {
+        deleteRecursivley( new File( "var/batch-insert") );
+    }
+    
+    private void deleteRecursivley( File file )
+    {
+        if ( file.isDirectory() )
+        {
+            for ( File child : file.listFiles() )
+            {
+                deleteRecursivley( child );
+            }
+        }
+        file.delete();
+    }
+    
+    public void testSimpleBatchInsert()
+    {
+        BatchInserter neo = new BatchInserterImpl( "var/batch-insert" );
+        LuceneIndexBatchInserter index = 
+            new LuceneIndexBatchInserterImpl( neo );
+        try
+        {
+            long node = neo.createNode( null );
+            assertTrue( !index.getNodes( "test-key", 
+                "test-value" ).iterator().hasNext() );
+            index.index( node, "test-key", "test-value" );
+            assertTrue( index.getNodes( "test-key", 
+                "test-value" ).iterator().hasNext() );
+        }
+        finally
+        {
+            index.shutdown();
+            neo.shutdown();
+        }
+    }
+}
