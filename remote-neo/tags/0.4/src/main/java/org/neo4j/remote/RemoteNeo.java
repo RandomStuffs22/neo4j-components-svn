b93ff1135ed273ea0e5e5b5b81b72d161ffeeb42
johans <johans@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2009-12-27 13:13:06 +0000

    Added tags:
    neo-1.0-b11
    shell-1.0-b11
    index-util-0.9
    remote-neo-0.4
    
    
    
    git-svn-id: https://svn.neo4j.org/components@3403 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ remote-neo/tags/0.4/src/main/java/org/neo4j/remote/RemoteNeo.java
@@ -0,0 +1,250 @@
+/*
+ * Copyright (c) 2008-2009 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ * 
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.remote;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
+
+import org.neo4j.api.core.NeoService;
+import org.neo4j.api.core.Node;
+import org.neo4j.api.core.Relationship;
+import org.neo4j.api.core.RelationshipType;
+import org.neo4j.api.core.Transaction;
+
+/**
+ * A remote connection to a running Neo instance, providing access to the Neo
+ * API.
+ * @author Tobias Ivarsson
+ */
+public final class RemoteNeo implements NeoService
+{
+    private static final ProtocolService protocol = new ProtocolService();
+
+    private final RemoteNeoEngine engine;
+
+    /**
+     * Creates a new remote Neo connection.
+     * @param site
+     *            The connection layer to be used.
+     */
+    public RemoteNeo( RemoteSite site )
+    {
+        this( null, site );
+    }
+
+    /**
+     * Creates a new remote Neo connection.
+     * @param config
+     *            the {@link ConfigurationModule} containing the configurations
+     *            of the subsystems of Neo.
+     * @param site
+     *            The connection layer to be used.
+     */
+    public RemoteNeo( ConfigurationModule config, RemoteSite site )
+    {
+        this( site.connect(), config );
+    }
+
+    /**
+     * Creates a new remote Neo connection.
+     * @param site
+     *            The connection layer to be used.
+     * @param username
+     *            the name of the user to log in as on the remote site. (
+     *            <code>null</code> means anonymous)
+     * @param password
+     *            the password for the user to log in as on the remote site.
+     */
+    public RemoteNeo( RemoteSite site, String username, String password )
+    {
+        this( null, site, username, password );
+    }
+
+    /**
+     * Creates a new remote Neo connection.
+     * @param config
+     *            the {@link ConfigurationModule} containing the configurations
+     *            of the subsystems of Neo.
+     * @param site
+     *            The connection layer to be used.
+     * @param username
+     *            the name of the user to log in as on the remote site. (
+     *            <code>null</code> means anonymous)
+     * @param password
+     *            the password for the user to log in as on the remote site.
+     */
+    public RemoteNeo( ConfigurationModule config, RemoteSite site,
+        String username, String password )
+    {
+        this( site.connect( username, password ), config );
+    }
+
+    /**
+     * Create a remote Neo connection. Select implementation depending on the
+     * supplied URI.
+     * @param resourceUri
+     *            the URI where the connection resource is located.
+     * @throws URISyntaxException
+     *             if the resource URI is malformed.
+     */
+    public RemoteNeo( String resourceUri ) throws URISyntaxException
+    {
+        this( null, resourceUri );
+    }
+
+    /**
+     * Create a remote Neo connection. Select implementation depending on the
+     * supplied URI.
+     * @param config
+     *            the {@link ConfigurationModule} containing the configurations
+     *            of the subsystems of Neo.
+     * @param resourceUri
+     *            the URI where the connection resource is located.
+     * @throws URISyntaxException
+     *             if the resource URI is malformed.
+     */
+    public RemoteNeo( ConfigurationModule config, String resourceUri )
+        throws URISyntaxException
+    {
+        this( config, protocol.get( new URI( resourceUri ) ) );
+    }
+
+    /**
+     * Create a remote Neo connection. Select implementation depending on the
+     * supplied URI.
+     * @param resourceUri
+     *            the URI where the connection resource is located.
+     * @param username
+     *            the name of the user to log in as on the remote site. (
+     *            <code>null</code> means anonymous)
+     * @param password
+     *            the password for the user to log in as on the remote site.
+     * @throws URISyntaxException
+     *             if the resource URI is malformed.
+     */
+    public RemoteNeo( String resourceUri, String username, String password )
+        throws URISyntaxException
+    {
+        this( null, resourceUri, username, password );
+    }
+
+    /**
+     * Create a remote Neo connection. Select implementation depending on the
+     * supplied URI.
+     * @param config
+     *            the {@link ConfigurationModule} containing the configurations
+     *            of the subsystems of Neo.
+     * @param resourceUri
+     *            the URI where the connection resource is located.
+     * @param username
+     *            the name of the user to log in as on the remote site. (
+     *            <code>null</code> means anonymous)
+     * @param password
+     *            the password for the user to log in as on the remote site.
+     * @throws URISyntaxException
+     *             if the resource URI is malformed.
+     */
+    public RemoteNeo( ConfigurationModule config, String resourceUri,
+        String username, String password ) throws URISyntaxException
+    {
+        this( config, protocol.get( new URI( resourceUri ) ), username,
+            password );
+    }
+
+    private RemoteNeo( RemoteConnection connection, ConfigurationModule config )
+    {
+        this.engine = new RemoteNeoEngine( connection, config );
+    }
+
+    /**
+     * Register a {@link RemoteSite} implementation with a specified protocol.
+     * @param factory
+     *            a factory to create the site once it's required.
+     */
+    public static void registerProtocol( RemoteSiteFactory factory )
+    {
+        protocol.register( factory );
+    }
+
+    // NeoService implementation
+
+    public Transaction beginTx()
+    {
+        return engine.beginTx();
+    }
+
+    public Node createNode()
+    {
+        return engine.current().createNode();
+    }
+
+    public Node getNodeById( long id )
+    {
+        // TODO: implement non-transactional path
+        return engine.current().getNodeById( id );
+    }
+
+    public Relationship getRelationshipById( long id )
+    {
+        // TODO: implement non-transactional path
+        return engine.current().getRelationshipById( id );
+    }
+
+    public Node getReferenceNode()
+    {
+        return engine.current( true ).getReferenceNode();
+    }
+
+    public Iterable<RelationshipType> getRelationshipTypes()
+    {
+        return engine.current( true ).getRelationshipTypes();
+    }
+
+    public Iterable<Node> getAllNodes()
+    {
+        return engine.current( true ).getAllNodes();
+    }
+
+    public void shutdown()
+    {
+        engine.shutdown();
+    }
+
+    RemoteNeoEngine getEngine()
+    {
+        return engine;
+    }
+
+    // These are scheduled to be removed from the NeoService interface.
+    public boolean enableRemoteShell()
+    {
+        // NOTE This might not be something that we wish to support in RemoteNeo
+        return false;
+    }
+
+    public boolean enableRemoteShell(
+        Map<String, Serializable> initialProperties )
+    {
+        // NOTE This might not be something that we wish to support in RemoteNeo
+        return false;
+    }
+}
