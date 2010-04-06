dee5f94a75802e0491d6476a680ea001d616dc66
andersn <andersn@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2010-04-06 10:11:44 +0000

    Fixed typo in javadoc.
    git-svn-id: https://svn.neo4j.org/components@4140 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- commons/trunk/src/main/java/org/neo4j/commons/iterator/IteratorAsIterable.java
+++ commons/trunk/src/main/java/org/neo4j/commons/iterator/IteratorAsIterable.java
@@ -3,7 +3,7 @@ package org.neo4j.commons.iterator;
 import java.util.Iterator;
 
 /**
- * Wraps an {@link Iterator}, making it look like an {@link Iterator}.
+ * Wraps an {@link Iterator}, making it look like an {@link Iterable}.
  * Important: It breaks the contract of the {@link Iterable} by returning the
  * same {@link Iterator} instance for every call to {@link #iterator()}.
  * 
