20a063eb8beb272b4405170f6048965ab84a15b3
mattias <mattias@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2010-04-19 10:34:56 +0000

    Added concept of "orphans". Values w/o keys associated to them
    git-svn-id: https://svn.neo4j.org/components@4253 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- commons/trunk/src/main/java/org/neo4j/commons/commandline/Args.java
+++ commons/trunk/src/main/java/org/neo4j/commons/commandline/Args.java
@@ -1,17 +1,43 @@
 package org.neo4j.commons.commandline;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Parses a String[] arguments from a main-method. It expects values to be
+ * either key/value pairs or just "orphan" values (w/o a key associated)
+ * 
+ * A key is defined with one or more dashes in the beginning, f.ex:
+ * 
+ *   '-path'
+ *   '--path'
+ * 
+ * A key/value pair can be either one single String from the array where
+ * there's a '=' delimiter between the key and value, like so:
+ * 
+ *   '--path=/my/path/to/something'
+ *   
+ * ...or consist of two (consecutive) strings from the array, like so:
+ * 
+ *   '-path' '/my/path/to/something'
+ */
 public class Args
 {
     private final String[] args;
-    private final Map<String, String> map;
+    private final Map<String, String> map = new HashMap<String, String>();
+    private final List<String> orphans = new ArrayList<String>();
     
     public Args( String[] args )
     {
         this.args = args;
-        this.map = parseArgs( args );
+        parseArgs( args );
+    }
+    
+    public String[] source()
+    {
+        return this.args;
     }
     
     public Map<String, String> asMap()
@@ -19,10 +45,14 @@ public class Args
         return new HashMap<String, String>( this.map );
     }
     
+    public boolean has( String  key )
+    {
+        return this.map.containsKey( key );
+    }
+    
     public String get( String key, String defaultValue )
     {
-        String value = this.map.get( key );
-        return value != null ? value : defaultValue;
+        return this.map.containsKey( key ) ? this.map.get( key ) : defaultValue;
     }
     
     public Number getNumber( String key, Number defaultValue )
@@ -35,6 +65,11 @@ public class Args
     {
         String value = this.map.get( key );
         return value != null ? Boolean.parseBoolean( value ) : defaultValue;
+    }
+    
+    public List<String> orphans()
+    {
+        return new ArrayList<String>( this.orphans );
     }
     
     private static boolean isOption( String arg )
@@ -51,9 +86,8 @@ public class Args
         return arg;
     }
 
-    private static Map<String, String> parseArgs( String[] args )
+    private void parseArgs( String[] args )
     {
-        Map<String, String> map = new HashMap<String, String>();
         for ( int i = 0; i < args.length; i++ )
         {
             String arg = args[ i ];
@@ -66,18 +100,24 @@ public class Args
                     String key = arg.substring( 0, equalIndex );
                     String value = arg.substring( equalIndex + 1 );
                     map.put( key, value );
+                    System.out.println( "put1:" + key + "=" + value );
                 }
                 else
                 {
                     String key = arg;
-                    int nextIndex = i + 1;
+                    int nextIndex = ++i;
                     String value = nextIndex < args.length ?
                         args[ nextIndex ] : null;
                     value = value == null || isOption( value ) ? null : value;
                     map.put( key, value );
+                    System.out.println( "put2:" + key + "=" + value );
                 }
             }
+            else
+            {
+                orphans.add( arg );
+                System.out.println( "orphan:" + arg );
+            }
         }
-        return map;
     }
 }
--- commons/trunk/src/test/java/org/neo4j/commons/commandline/TestArgs.java
+++ commons/trunk/src/test/java/org/neo4j/commons/commandline/TestArgs.java
@@ -2,6 +2,8 @@ package org.neo4j.commons.commandline;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 import org.neo4j.commons.collection.MapUtil;
 
@@ -10,9 +12,10 @@ public class TestArgs
     @Test
     public void testArgsParser()
     {
-        Args args = new Args( new String[] { "-port=9999", "-type", "test-type",
-                "--longer-name=the name", "--some-option", "hi there" } );
+        Args args = new Args( new String[] { "-port=9999", "-type", "test-type", "orphan1",
+                "--longer-name=the name", "--some-option", "hi there", "orphan2" } );
         assertEquals( MapUtil.of( "port", "9999", "type", "test-type",
                 "longer-name", "the name", "some-option", "hi there" ), args.asMap() );
+        assertEquals( Arrays.asList( "orphan1", "orphan2" ), args.orphans() );
     }
 }
