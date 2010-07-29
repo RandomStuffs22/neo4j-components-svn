b74938a6b9fc978783ac5eeb1ced8c52e9f8008c
jespern <jespern@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2010-07-29 18:06:24 +0000

    [maven-release-plugin]  copy for tag 1.1
    git-svn-id: https://svn.neo4j.org/components@4963 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ index/tags/1.1/src/main/java/org/neo4j/index/lucene/LuceneFulltextIndexService.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2002-2009 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ * 
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.lucene;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.Token;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.Node;
+import org.neo4j.index.IndexHits;
+
+/**
+ * A {@link LuceneIndexService} which indexes the values with fulltext indexing.
+ * Fulltext means that the indexing process takes the values you throw in and
+ * tokenizes those into words so that you can query for those individual words
+ * in {@link #getNodes(String, Object)}. Also queries are case-insensitive.
+ * 
+ * It stores more data per Lucene entry to make this possible. This makes it
+ * incompatible with {@link LuceneIndexService} so it has got its own XA
+ * resource ID. This means that you can have one {@link LuceneIndexService} and
+ * one {@link LuceneFulltextIndexService} for a {@link GraphDatabaseService}.
+ * 
+ * See more information at
+ * http://wiki.neo4j.org/content/Indexing_with_IndexService#Fulltext_indexing
+ */
+public class LuceneFulltextIndexService extends LuceneIndexService
+{
+    protected static final String DOC_INDEX_SOURCE_KEY = "index_source";
+    protected static final String FULLTEXT_DIR_NAME_POSTFIX = "-fulltext";
+
+    /**
+     * @param graphDb the {@link GraphDatabaseService} to use.
+     */
+    public LuceneFulltextIndexService( GraphDatabaseService graphDb )
+    {
+        super( graphDb );
+    }
+
+    @Override
+    protected Class<? extends LuceneDataSource> getDataSourceClass()
+    {
+        return LuceneFulltextDataSource.class;
+    }
+
+    @Override
+    protected String getDirName()
+    {
+        return super.getDirName() + FULLTEXT_DIR_NAME_POSTFIX;
+    }
+
+    @Override
+    protected byte[] getXaResourceId()
+    {
+        return "262374".getBytes();
+    }
+
+    /**
+     * Since this is a "fulltext" index it changes the contract of this method
+     * slightly. It treats the {@code value} more like a query in than you can
+     * query for individual words in your indexed values.
+     * 
+     * So if you've indexed node (1) with value "Andy Wachowski" and node (2)
+     * with "Larry Wachowski" you can expect this behaviour if you query for:
+     * 
+     * <ul>
+     * <li>"addy" --> (1)</li>
+     * <li>"Andy" --> (1)</li>
+     * <li>"wachowski" --> (1), (2)</li>
+     * <li>"andy larry" --></li>
+     * <li>"larry Wachowski" --> (2)</li>
+     * <li>"wachowski Andy" --> (1)</li>
+     * </ul>
+     */
+    @Override
+    public IndexHits<Node> getNodes( String key, Object value )
+    {
+        return super.getNodes( key, value );
+    }
+    
+    /**
+     * Does a {@link #getNodes(String, Object)} using exact matching, so that
+     * it for this call behaves like {@link LuceneIndexService}.
+     * @param key the key.
+     * @param value the query.
+     * @return the result of the query.
+     */
+    @Override
+    public IndexHits<Node> getNodesExactMatch( String key, Object value )
+    {
+        return getNodes( key, value, MatchingType.EXACT, null );
+    }
+    
+    @Override
+    public Node getSingleNodeExactMatch( String key, Object value )
+    {
+        return getSingleNode( key, value, MatchingType.EXACT );
+    }
+
+    @Override
+    protected Query formQuery( String key, Object value, Object matching )
+    {
+        if ( matching == MatchingType.EXACT )
+        {
+            return new TermQuery( new Term( DOC_INDEX_SOURCE_KEY, value.toString() ) );
+        }
+        
+        TokenStream stream = LuceneFulltextDataSource.LOWER_CASE_WHITESPACE_ANALYZER.tokenStream(
+                DOC_INDEX_KEY,
+                new StringReader( value.toString().toLowerCase() ) );
+        Token token = new Token();
+        BooleanQuery booleanQuery = new BooleanQuery();
+        try
+        {
+            while ( ( token = stream.next( token ) ) != null )
+            {
+                String term = token.term();
+                booleanQuery.add(
+                        new TermQuery( new Term( DOC_INDEX_KEY, term ) ),
+                        Occur.MUST );
+            }
+        }
+        catch ( IOException e )
+        {
+            throw new RuntimeException( e );
+        }
+        return booleanQuery;
+    }
+
+    @Override
+    public void enableCache( String key, int maxNumberOfCachedEntries )
+    {
+        // For now, or is it just not feasable
+        throw new UnsupportedOperationException();
+    }
+    
+    static enum MatchingType
+    {
+        DEFAULT,
+        EXACT
+    }
+}
---
+++ index/tags/1.1/src/test/java/org/neo4j/index/lucene/TestCustomLuceneFulltextIndexService.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2002-2009 "Neo Technology,"
+ *     Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * This file is part of Neo4j.
+ * 
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.index.lucene;
+
+import org.junit.Ignore;
+import org.neo4j.graphdb.Node;
+import org.neo4j.index.IndexService;
+
+public class TestCustomLuceneFulltextIndexService
+    extends TestLuceneIndexingService
+{
+    @Override
+    protected IndexService instantiateIndex()
+    {
+        return new LuceneFulltextQueryIndexService( graphDb() );
+    }
+    
+    @Override
+    @Ignore
+    public void testCaching()
+    {
+        // Do nothing
+    }
+    
+    @Override
+    @Ignore
+    public void testGetNodesBug()
+    {
+        // Do nothing
+    }
+
+    @Override
+    @Ignore
+    public void testRemoveAllWithCache()
+    {
+        // Do nothing
+    }
+    
+    public void testCustomFulltext() throws Exception
+    {
+        Node node1 = graphDb().createNode();
+        Node node2 = graphDb().createNode();
+        
+        String key1 = "lastName";
+        String key2 = "modifiedTime";
+
+        index().index( node1, key1, "Smith" );
+        index().index( node2, key1, "Mattias Smith" );
+        index().index( node2, key2, "2009" );
+        index().index( node1, key2, "449854" );
+        
+        assertCollection( asCollection(
+            index().getNodes( key1, "smith" ) ), node1, node2 );
+        assertCollection( asCollection(
+            index().getNodes( key1, "smish~" ) ), node1, node2 );
+        assertCollection( asCollection(
+            index().getNodes( key2, "[2010 TO >]" ) ), node1 );
+        
+        restartTx();
+        
+        assertCollection( asCollection(
+            index().getNodes( key1, "smith" ) ), node1, node2 );
+        assertCollection( asCollection(
+            index().getNodes( key1, "smish~" ) ), node1, node2 );
+        assertCollection( asCollection(
+            index().getNodes( key2, "[2010 TO >]" ) ), node1 );
+    }
+
+    public void testSpecific() throws Exception
+    {
+        Node andy = graphDb().createNode();
+        Node larry = graphDb().createNode();
+        String key = "atest";
+        index().index( andy, key, "Andy Wachowski" );
+        index().index( larry, key, "Larry Wachowski" );
+        
+        assertCollection( asCollection(
+            index().getNodes( key, "+andy +wachowski" ) ), andy );
+        assertCollection( asCollection(
+            index().getNodes( key, "+Andy +Wachowski" ) ), andy );
+        assertCollection( asCollection(
+            index().getNodes( key, "andy" ) ), andy );
+        assertCollection( asCollection(
+            index().getNodes( key, "Andy" ) ), andy );
+        assertCollection( asCollection(
+            index().getNodes( key, "larry" ) ), larry );
+        assertCollection( asCollection(
+            index().getNodes( key, "andy larry" ) ), andy, larry );
+        assertCollection( asCollection(
+            index().getNodes( key, "+andy +larry" ) ) );
+        assertCollection( asCollection(
+            index().getNodes( key, "wachow*" ) ), andy, larry );
+    }
+
+    @Override
+    public void testChangeValueBug() throws Exception
+    {
+        Node andy = graphDb().createNode();
+        Node larry = graphDb().createNode();
+
+        andy.setProperty( "name", "Andy Wachowski" );
+        andy.setProperty( "title", "Director" );
+        // Deliberately set Larry's name wrong
+        larry.setProperty( "name", "Andy Wachowski" );
+        larry.setProperty( "title", "Director" );
+        index().index( andy, "name", andy.getProperty( "name" ) );
+        index().index( andy, "title", andy.getProperty( "title" ) );
+        index().index( larry, "name", larry.getProperty( "name" ) );
+        index().index( larry, "title", larry.getProperty( "title" ) );
+    
+        // Correct Larry's name
+        index().removeIndex( larry, "name",
+            larry.getProperty( "name" ) );
+        larry.setProperty( "name", "Larry Wachowski" );
+        index().index( larry, "name",
+            larry.getProperty( "name" ) );
+    
+        assertCollection( asCollection( index().getNodes(
+            "name", "Andy AND Wachowski" ) ), andy );
+        assertCollection( asCollection( index().getNodes(
+            "name", "Larry AND Wachowski" ) ), larry );
+    }
+}
