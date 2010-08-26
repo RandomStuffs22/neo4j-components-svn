cd0fbc109af2f2ae9773742b00aedacd7b6bd961
andersn <andersn@0b971d98-bb2f-0410-8247-b05b2b5feb2a>
2010-08-26 10:23:26 +0000

    Removing broken release.
    git-svn-id: https://svn.neo4j.org/components@5442 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- kernel/tags/1.1.1/src/test/java/org/neo4j/kernel/impl/traversal/GraphDefinition.java
+++
@@ -1,9 +0,0 @@
-package org.neo4j.kernel.impl.traversal;
-
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Node;
-
-public interface GraphDefinition
-{
-    Node create( GraphDatabaseService graphdb );
-}
